94eaff0b2559401dea08f5b05027d4dbc1d2ecbf
orelnatan <natenorel@gmail.com>
2018-10-16 00:39:10 +0300

    app root comment remove

--- src/app/appRoot.component.ts
+++ src/app/appRoot.component.ts
@@ -8,19 +8,3 @@ import { Component } from '@angular/core';
 
 // This is the top level component...
 export class AppRoot {}
-
-
-
-//////////////////////////////////////////////////////////////
-
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './appRoot.component.html',
-//   styleUrls: ['./appRoot.component.scss']
-// })
-
-// export class AppRoot {
-  
-// }
